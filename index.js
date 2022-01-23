ea59ebc22f7c7de7d70ce0f0250e8c920d9cb0c9
Anishh <anishsh701@gmail.com>
2022-01-23 10:51:00 +0530

    express updated

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 5000
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
